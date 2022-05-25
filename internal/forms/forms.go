cdbfb8aab5b2f45cd03a10eb709c5af4d2955aaf
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-25 19:31:46 +0100

    serverside validation - checks for valid email address

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/asaskevich/govalidator"
 )
 
 // Form creates a custom form struct and embeds a url.Values object.
@@ -59,4 +61,11 @@ func (f *Form) MinLength(field string, length int, r *http.Request) bool {
 	}
 
 	return true
+}
+
+// IsEmail checks if email field data is a valid email address
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "invalid email address")
+	}
 }
