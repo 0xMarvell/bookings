bf4580b4eef2d8356872531f70006d74c864b932
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-25 19:08:24 +0100

    serverside validation-checks length of field input

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,14 +1,26 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form creates a custom form struct and embeds a url.Values object.
 type Form struct {
 	url.Values
 	Errors errors
+}
+
+// Required checks if the required form fields have the right value
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
 }
 
 // Valid returns true if there are no error in inputted form data,
@@ -29,7 +41,20 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This field cannot be blank")
+		// f.Errors.Add(field, "This field cannot be blank")
+		return false
+	}
+
+	return true
+}
+
+// Minlength checks if the inputted field data surpasses
+// the minimum length of data expected.
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	fieldData := r.Form.Get(field)
+
+	if len(fieldData) < length {
+		f.Errors.Add(field, fmt.Sprintf("this field must be at least %d characters long", length))
 		return false
 	}
 
